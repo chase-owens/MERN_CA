a7e068018c52361b8fc1be3f65f56e0719ad1e63
Chase-Owens <chasejonathanowens@gmail.com>
2019-05-05 18:30:13 -0500

    readyForProd

--- src/server/httpRequests.js
+++ src/server/httpRequests.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const url = (process.env.NODE_ENV = 'production'
-  ? ``
-  : 'http://localhost:1221/');
+const url =
+  process.env.NODE_ENV == 'production' ? `` : 'http://localhost:1221/';
 
 export const getDataFromServer = async () => {
   let endpoint = `${url}data`;
