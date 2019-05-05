6c49c860d809d76e88ed2685882a8eadc1bdceb5
Chase-Owens <chasejonathanowens@gmail.com>
2019-05-05 10:06:00 -0500

    Initialize DB on Load

--- src/server/initialize-db.js
+++ src/server/initialize-db.js
@@ -1,12 +1,12 @@
 import { defaultState } from './defaultState';
 import { connectDB } from './connect-db';
 
-async function initializeDB() {
+const initializeDB = async () => {
   let db = await connectDB();
   for (let collectionName in defaultState) {
     let collection = db.collection(collectionName);
     await collection.insertMany(defaultState[collectionName]);
   }
-}
+};
 
 initializeDB();
