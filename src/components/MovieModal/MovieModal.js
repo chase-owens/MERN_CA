07a2393ecb7ccc603989b240e45c4a974d4e8fe4
Chase-Owens <chasejonathanowens@gmail.com>
2019-06-26 21:53:08 -0500

    movieModalGroom

--- src/components/MovieModal/MovieModal.js
+++ src/components/MovieModal/MovieModal.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import withWidth from '@material-ui/core/withWidth';
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery';
+
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { toggleMovie } from '../Videos/videos.action';
@@ -22,29 +29,74 @@ const MovieModal = ({ focusedVideo, toggleMovie }) => {
           width: '100vw',
           height: '100vh',
           // opacity: 0.7,
-          background: theme.palette.ternary.main,
+          // background: theme.palette.ternary.main,
           position: 'fixed',
           top: 0
         }}
       >
         <div
           style={{
-            width: '90%',
-            height: '90%',
-            margin: 'auto',
-            marginTop: '5vh'
-            // paddingTop: '2.5%'
+            width: '100vw',
+            height: '100vh',
+            opacity: 0.7,
+            background: theme.palette.ternary.main,
+            position: 'relative',
+            top: 0
+          }}
+        >
+          {useMediaQuery(theme.breakpoints.up(740)) && (
+            <Grid container justify='flex-end'>
+              <Grid item>
+                <Button
+                  style={{
+                    position: 'fixed',
+                    color: '#fff',
+                    zIndex: 999,
+                    top: 3,
+                    right: 3
+                  }}
+                  onClick={() => toggleMovie(null)}
+                >
+                  <Icon
+                    style={{
+                      paddingBottom: 15,
+                      paddingTop: -15,
+                      paddingRight: 20,
+                      marginRight: -12,
+                      marginLeft: -5
+                    }}
+                  >
+                    <i style={{ fontSize: 40 }} class='material-icons'>
+                      close
+                    </i>
+                  </Icon>
+                </Button>
+              </Grid>
+            </Grid>
+          )}
+        </div>
+        <div
+          style={{
+            width: '90vw',
+            height: '90vh',
+            // opacity: 0.7,
+            // background: theme.palette.ternary.main,
+            position: 'fixed',
+            top: 0,
+            left: 0,
+
+            marginTop: '5vh',
+            marginLeft: '5vh'
           }}
         >
           <ReactPlayer
             style={{
               marginTop: 0,
-              padding: 0
+              padding: 0,
+              zIndex: 1800
             }}
             url={focusedVideo}
             playing
-            volume={0}
-            muted
             loop
             width='100%'
             height='100%'
@@ -55,7 +107,9 @@ const MovieModal = ({ focusedVideo, toggleMovie }) => {
   );
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(MovieModal);
+export default withWidth({ withTheme: true })(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(MovieModal)
+);
