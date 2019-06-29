5c71ccaa24b92c8267eb39adba5e348961b487fe
Chase-Owens <chasejonathanowens@gmail.com>
2019-06-28 23:36:25 -0500

    debug

--- src/components/NavDropdown/NavPage.js
+++ src/components/NavDropdown/NavPage.js
@@ -25,7 +25,6 @@ const NavDropdown = ({ language, open, theme }) => {
         width: '100%',
         height: '100%',
         zIndex: open ? 500 : 5,
-        marginLeft: open ? 0 : 40,
         transition: 'all .18s ease-in-out'
       }}
     >
