29c3c5db2d750a49e1ff0a12931f531900b84aac
Chase-Owens <chasejonathanowens@gmail.com>
2019-07-01 12:03:36 -0500

    D

--- src/components/NavButtons/NavButtons.js
+++ src/components/NavButtons/NavButtons.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import navButtons from '../Nav/navOptions.config';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 import { withTheme } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { theme } from '../../styles/theme';
@@ -13,23 +14,32 @@ const NavButtons = ({ direction }) => {
       container
       direction={direction}
       justify='space-between'
-      style={{ width: '100%', margin: direction === 'column' ? 40 : null }}
+      style={{
+        width: direction === 'column' ? '80%' : '100%',
+        margin: direction === 'column' ? 'auto' : null,
+        marginTop: direction === 'column' ? 125 : null
+      }}
     >
       {navButtons.map(button => (
         <Grid item key={button.title}>
           <Link
-            style={{ textDecoration: 'none', margin: '0 5px' }}
+            style={{
+              textDecoration: 'none'
+            }}
             to={button.location}
           >
             <Button
               style={{
                 color: direction === 'column' ? theme.palette.text.light : null,
-                fontSize: isMobile ? '2.5em' : null
+                fontSize: direction === 'column' ? '2.5em' : null,
+                display: 'block',
+                margin: 'auto'
               }}
             >
               {button.title}
             </Button>
           </Link>
+          {direction === 'column' && <Divider />}
         </Grid>
       ))}
     </Grid>
