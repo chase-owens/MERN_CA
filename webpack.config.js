8e1fe3236a17079607eb7c7762cdc917222ea10f
Chase-Owens <chasejonathanowens@gmail.com>
2019-08-06 21:48:49 -0500

    static-site-generator-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/'
-    // libraryTarget: 'umd',
-    // globalObject: 'this'
+    libraryTarget: 'umd',
+    globalObject: 'this'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.css'],
@@ -27,13 +27,13 @@ module.exports = {
     host: 'localhost'
   },
   plugins: [
-    // new StaticSiteGeneratorPlugin({
-    //   crawl: true,
-    //   entry: path.resolve(__dirname, 'dist'),
-    //   globals: {
-    //     window: {}
-    //   }
-    // })
+    new StaticSiteGeneratorPlugin({
+      crawl: true,
+      entry: 'main',
+      // globals: {
+      //   window: {}
+      // }
+    })
   ],
   module: {
     rules: [
