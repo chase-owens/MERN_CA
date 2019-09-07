b2aa9b84cbc9d186331896770596d03187c737cc
Chase-Owens <chasejonathanowens@gmail.com>
2019-09-07 12:05:12 -0500

    vid

--- src/components/MovieModal/MovieModal.js
+++ src/components/MovieModal/MovieModal.js
@@ -34,88 +34,102 @@ const MovieModal = ({ focusedVideo, toggleMovie }) => {
       <source src={focusedVideo} />
     </video>
   );
-  return isMobile && focusedVideo !== null
-    ? { iframe }
-    : !isMobile && focusedVideo !== null && (
+  return isMobile && focusedVideo !== null ? (
+    <ReactPlayer
+      style={{
+        marginTop: 0,
+        padding: 0,
+        zIndex: 1800
+      }}
+      url={focusedVideo}
+      playing
+      loop
+      width='100%'
+      height='100%'
+      controls
+    />
+  ) : (
+    !isMobile && focusedVideo !== null && (
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          // opacity: 0.7,
+          // background: theme.palette.ternary.main,
+          position: 'fixed',
+          top: 0
+        }}
+      >
         <div
           style={{
             width: '100vw',
             height: '100vh',
-            // opacity: 0.7,
-            // background: theme.palette.ternary.main,
-            position: 'fixed',
+            opacity: 0.7,
+            background: theme.palette.ternary.main,
+            position: 'relative',
             top: 0
           }}
         >
-          <div
-            style={{
-              width: '100vw',
-              height: '100vh',
-              opacity: 0.7,
-              background: theme.palette.ternary.main,
-              position: 'relative',
-              top: 0
-            }}
-          >
-            {useMediaQuery(theme.breakpoints.up(740)) && (
-              <Grid container justify='flex-end'>
-                <Grid item>
-                  <Button
+          {useMediaQuery(theme.breakpoints.up(740)) && (
+            <Grid container justify='flex-end'>
+              <Grid item>
+                <Button
+                  style={{
+                    position: 'fixed',
+                    color: '#fff',
+                    zIndex: 999,
+                    top: 3,
+                    right: 3
+                  }}
+                  onClick={() => toggleMovie(null)}
+                >
+                  <Icon
                     style={{
-                      position: 'fixed',
-                      color: '#fff',
-                      zIndex: 999,
-                      top: 3,
-                      right: 3
+                      paddingBottom: 15,
+                      paddingTop: -15,
+                      paddingRight: 20,
+                      marginRight: -12,
+                      marginLeft: -5
                     }}
-                    onClick={() => toggleMovie(null)}
                   >
-                    <Icon
-                      style={{
-                        paddingBottom: 15,
-                        paddingTop: -15,
-                        paddingRight: 20,
-                        marginRight: -12,
-                        marginLeft: -5
-                      }}
-                    >
-                      <i style={{ fontSize: 40 }} class='material-icons'>
-                        close
-                      </i>
-                    </Icon>
-                  </Button>
-                </Grid>
+                    <i style={{ fontSize: 40 }} class='material-icons'>
+                      close
+                    </i>
+                  </Icon>
+                </Button>
               </Grid>
-            )}
-          </div>
-          <div
-            style={{
-              width: '90vw',
-              height: '90vh',
-              position: 'fixed',
-              top: 0,
-              left: 0,
-
-              marginTop: '5vh',
-              marginLeft: '5vh'
-            }}
-          >
-            <ReactPlayer
-              style={{
-                marginTop: 0,
-                padding: 0,
-                zIndex: 1800
-              }}
-              url={focusedVideo}
-              playing
-              loop
-              width='100%'
-              height='100%'
-              controls
-            />
-          </div>
+            </Grid>
+          )}
         </div>
-      );
+        <div
+          style={{
+            width: '90vw',
+            height: '90vh',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+
+            marginTop: '5vh',
+            marginLeft: '5vh'
+          }}
+        >
+          <ReactPlayer
+            style={{
+              marginTop: 0,
+              padding: 0,
+              zIndex: 1800
+            }}
+            url={focusedVideo}
+            playing
+            loop
+            width='100%'
+            height='100%'
+            controls
+          />
+        </div>
+      </div>
+    )
+  );
 };
 
 export default withWidth({ withTheme: true })(
