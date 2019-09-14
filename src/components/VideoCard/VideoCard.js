601ede49e684243e6d6690eec9d6bce753775c37
Chase-Owens <chasejonathanowens@gmail.com>
2019-09-14 15:28:58 -0500

    iframe

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -262,6 +262,14 @@ const VideoCard = ({
             </Icon>
           </div>
         </span>
+        <iframe
+          width='560'
+          height='315'
+          src={video.videoPath}
+          frameborder='0'
+          allow='accelerometer; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        ></iframe>
       </div>
 
       <br />
