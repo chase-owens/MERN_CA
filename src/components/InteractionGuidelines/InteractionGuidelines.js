93699f33f01d6c3541b02d51bd5274a6ec33d8cb
Chase-Owens <chasejonathanowens@gmail.com>
2019-09-21 23:09:37 -0500

    responsiveGrooming

--- src/components/InteractionGuidelines/InteractionGuidelines.js
+++ src/components/InteractionGuidelines/InteractionGuidelines.js
@@ -10,7 +10,7 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   text: {
-    fontSize: '1em'
+    // fontSize: '1em'
   }
 });
 
